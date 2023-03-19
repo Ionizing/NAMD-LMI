89b4c1c75879ab65591653fc78548707eb388ac4
Ionizing <ljchen9@mail.ustc.edu.cn>
2023-03-19 21:48:18 +0800

    [make.inc] provide make.inc for ifort, common.h changed with it [README.md] change TODO

--- src/common.h
+++ src/common.h
@@ -2,4 +2,10 @@
 #define TFINT2STR(x,fmt) TRIM(int2str(x,fmt))
 #define TREAL2STR(x) TRIM(real2str(x))
 #define AT "at " // __FILE__ // ":" // TINT2STR(__LINE__)
+
+#ifdef __INTEL_COMPILER
+#define REALPART DREAL
+#define IMAGPART DIMAG
+#else
 #define STOP CALL BACKTRACE; stop
+#endif
