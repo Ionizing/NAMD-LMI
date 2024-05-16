399d368ed9454e27763f2606a31a2a0615ee3388
Ionizing <ljchen9@mail.ustc.edu.cn>
2024-05-17 00:04:01 +0800

    [efield.rs] refactoring [hamil_impl.rs] io done

--- namd/src/hamil/efield.rs
+++ namd/src/hamil/efield.rs
@@ -1,20 +1,14 @@
-use std::fs::{read_to_string, File};
+use std::fs::{
+    File,
+    read_to_string,
+};
 use std::io::{
     Write as _,
     BufWriter,
 };
-use std::path::{
-    Path,
-    PathBuf,
-};
+use std::path::Path;
+use std::sync::OnceLock;
 
-use shared::{
-    info,
-    warn,
-    MatX3,
-    Context,
-    Result,
-};
 use rhai::{
     AST,
     Dynamic,
@@ -23,88 +17,108 @@ use rhai::{
     Scope,
 };
 
+use shared::{
+    info,
+    warn,
+    anyhow::ensure,
+    Context,
+    Result,
+};
+use shared::MatX3;
 
-#[derive(Clone)]
-pub struct Efield {
-    raw: String,
-    ast: AST,
-    namdtime: usize,
-    nelm: usize,
-    dt: f64,
-    t: Vec<f64>,
-    efield: MatX3<f64>,
-    afield: MatX3<f64>,
+
+const ENGINE: OnceLock<Engine> = <_>::default();
+fn get_engine() -> &'static Engine {
+    ENGINE.get_or_init(|| Engine::new())
 }
 
 
-impl Efield {
-    pub fn from_file(fname: PathBuf) -> Self
-    {
-        let raw = read_to_string(fname).unwrap();
-        let engine = Engine::new();
-        let ast = engine.compile_file(fname.clone())
-            .with_context(|| format!("Cannot load script {:?}", fname))
-            .unwrap();
-
-        Self {
-            raw,
-            ast,
-            namdtime: 0,
-            nelm: 0,
-            dt: 0.0,
-            t: vec![],
-            efield: vec![],
-            afield: vec![],
-        }
-    }
+#[derive(Clone)]
+pub struct Efield<'a> {
+    raw: String,
+    ast: AST,
+    scope: Scope<'a>,
+}
 
 
-    pub fn initialize(&mut self, namdtime: usize, nelm: usize, dt: f64) {
-        if self.namdtime == namdtime  &&
-           self.nelm == nelm &&
-           self.dt == dt   &&
-           self.t.len() == namdtime * nelm &&
-           self.efield.len() == namdtime * nelm &&
-           self.afield.len() == namdtime * nelm {
-            return;
-        }
+impl<'a> Efield<'a> {
+    pub fn from_str(raw: &str) -> Result<Self> {
+        let raw = raw.to_string();
 
-        self.namdtime  = namdtime;
-        self.nelm = nelm;
-        self.dt   = dt;
-
-        let engine = Engine::new();
+        let engine = get_engine();
+        let ast = engine.compile(&raw).context("Failed eval efield.")?;
         let mut scope = Scope::new();
         scope.push_constant("hbar", 0.6582119569);
         scope.push_constant("pi", std::f64::consts::PI);
         scope.push_constant("e", std::f64::consts::E);
 
-        let mut f = |t: f64| {
-            let ret = engine.call_fn::<Dynamic>(&mut scope, &self.ast, "efield", (t,))
-                .unwrap()
-                .into_typed_array::<FLOAT>()
-                .unwrap();
-            assert_eq!(3, ret.len());
-            
-            [ret[0], ret[1], ret[2]]
-        };
+        Ok(Self {
+            raw,
+            ast,
+            scope,
+        })
+    }
 
+
+    pub fn from_file<P>(fname: P) -> Result<Self>
+    where P: AsRef<Path> {
+        let raw = read_to_string(fname)?;
+        Self::from_str(&raw)
+    }
+
+
+    pub fn get_src(&self) -> &str {
+        &self.raw
+    }
+
+
+    pub fn eval(&mut self, t: f64) -> [f64; 3] {
+        let engine = get_engine();
+        let ast    = &self.ast;
+        let scope  = &mut self.scope;
+
+        let ret = engine.call_fn::<Dynamic>(scope, ast, "efield", (t,))
+            .unwrap()
+            .into_typed_array::<FLOAT>()
+            .unwrap();
+        assert_eq!(3, ret.len());
+        
+        [ret[0], ret[1], ret[2]]
+    }
+
+
+    pub fn eval_array(&mut self, ts: &[f64]) -> MatX3<f64> {
+        let engine = get_engine();
+        let ast    = &self.ast;
+        let scope  = &mut self.scope;
+
+        ts.iter().cloned()
+            .map(|t| -> [f64; 3] {
+                let ret = engine.call_fn::<Dynamic>(scope, ast, "efield", (t,))
+                    .unwrap()
+                    .into_typed_array::<FLOAT>()
+                    .unwrap();
+                [ret[0], ret[1], ret[3]]
+            })
+            .collect()
+    }
+
+
+    pub fn get_eafield_array(&mut self, namdtime: usize, potim: f64, nelm: usize) -> (Vec<f64>, [MatX3<f64>; 2]) {
+        let edt = potim / nelm as f64;
         let mut tt = vec![0.0; 0];
-        let mut efield = vec![[0.0; 3]; 0];
-        let mut afield = vec![[0.0; 3]; 0];
+        let mut efield = MatX3::<f64>::new();
+        let mut afield = MatX3::<f64>::new();
 
-        let edt = dt / nelm as f64;
-
-        // get electric field
         for iion in 0 .. namdtime {
-            let t0 = dt * iion as f64;
+            let t0 = potim * iion as f64;
             for ielm in 0 .. nelm {
-                let t = t0 + edt * ielm as f64;
-
+                let t = t0 + edt * nelm as f64;
                 tt.push(t);
-                efield.push(f(t));
             }
         }
+
+        efield = self.eval_array(&tt);
 
         // integrate electric field into vector potential
         // E = - \partial A / \partial t
@@ -117,72 +131,57 @@ impl Efield {
             afield.push(at);
         }
 
-        self.t      = tt;
-        self.efield = efield;
-        self.afield = afield;
+        (tt, [efield, afield])
     }
 
 
-    /// Get electric field E, in V/Å
-    pub fn get_efield(&self, iion: usize, ielm: usize) -> [f64; 3] {
-        let i = iion * self.nelm + ielm;
-        self.efield[i]
-    }
+    pub fn print_efield_tofile<P>(&self, dir: P, namdtime: usize, potim: f64, nelm: usize) -> Result<()>
+    where P: AsRef<Path> {
+        let dir = dir.as_ref();
+        ensure!(dir.is_dir(), "The parameter dir should be a valid directory.");
+        let efield_fname = dir.with_file_name("EFIELD.txt");
+        let afield_fname = dir.with_file_name("AFIELD.txt");
+
+        let (t, [efield, afield]) = self.get_eafield_array(namdtime, potim, nelm);
 
 
-    /// Get vector potential A, in V*fs/Å
-    pub fn get_afield(&self, iion: usize, ielm: usize) -> [f64; 3] {
-        let i = iion * self.nelm + ielm;
-        self.afield[i]
-    }
+        {
+            info!("Writing electric field to {:?} ...", efield_fname);
+            if efield_fname.is_file() {
+                warn!("File {:?} exists, overwriting ...", efield_fname);
+            }
 
+            let mut f = BufWriter::new(
+                File::options().write(true)
+                               .create(true)
+                               .truncate(true)
+                               .open(efield_fname)?
+                );
 
-    pub fn print_efield_to_file<P>(&self, fname: P) -> Result<()>
-    where
-        P: AsRef<Path>,
-    {
-        info!("Writing electric field to {:?} ...", fname.as_ref());
-        if fname.as_ref().is_file() {
-            warn!("File {:?} already exists, overwriting ...", fname.as_ref());
+            writeln!(f, "# Time(fs) Ex Ey Ez(V/Å)")?;
+            for (i, e) in efield.iter().enumerate() {
+                writeln!(f, "{:15.5} {:15.7} {:15.7} {:15.7}", t[i], e[0], e[1], e[2])?;
+            }
         }
 
-        let mut f = BufWriter::new(
-            File::options().write(true)
-                           .create(true)
-                           .truncate(true)
-                           .open(fname)?
-            );
 
-        writeln!(f, "# Time(fs) Ex Ey Ez(V/Å)")?;
-        
-        for (i, e) in self.efield.iter().enumerate() {
-            writeln!(f, "{:15.5} {:15.7} {:15.7} {:15.7}", self.t[i], e[0], e[1], e[2])?;
-        }
+        {
+            info!("Writing integrated vector potential to {:?} ...", afield_fname);
+            if efield_fname.is_file() {
+                warn!("File {:?} exists, overwriting ...", afield_fname);
+            }
 
-        Ok(())
-    }
+            let mut f = BufWriter::new(
+                File::options().write(true)
+                               .create(true)
+                               .truncate(true)
+                               .open(afield_fname)?
+                );
 
-
-    pub fn print_afield_to_file<P>(&self, fname: P) -> Result<()>
-    where
-        P: AsRef<Path>,
-    {
-        info!("Writing integrated vector potential to {:?} ...", fname.as_ref());
-        if fname.as_ref().is_file() {
-            warn!("File {:?} already exists, overwriting ...", fname.as_ref());
-        }
-
-        let mut f = BufWriter::new(
-            File::options().write(true)
-                           .create(true)
-                           .truncate(true)
-                           .open(fname)?
-            );
-
-        writeln!(f, "# Time(fs) Ax Ay Az(V*fs/Å)")?;
-        
-        for (i, a) in self.afield.iter().enumerate() {
-            writeln!(f, "{:15.5} {:15.7} {:15.7} {:15.7}", self.t[i], a[0], a[1], a[2])?;
+            writeln!(f, "# Time(fs) Ax Ay Az(V*fs/Å)")?;
+            for (i, a) in afield.iter().enumerate() {
+                writeln!(f, "{:15.5} {:15.7} {:15.7} {:15.7}", t[i], a[0], a[1], a[2])?;
+            }
         }
 
         Ok(())
--- namd/src/hamil/hamil_impl.rs
+++ namd/src/hamil/hamil_impl.rs
@@ -7,15 +7,12 @@ use shared::{c64, Result};
 
 use crate::core::Hamiltonian;
 use crate::nac::nac_impl::Nac;
-use crate::hamil::config::{
-    HamilConfig,
-    PropagateMethod,
-};
+use crate::hamil::config::HamilConfig;
 use crate::hamil::efield::Efield;
 
 
 /// Sing-Particle Hamiltonian
-pub struct SPHamiltonian {
+pub struct SPHamiltonian<'a> {
     ikpoint:     usize,
     basis_up:    [usize; 2],
     basis_dn:    [usize; 2],
@@ -29,13 +26,11 @@ pub struct SPHamiltonian {
     pij_t:     nd::Array4<c64>,     // [nsw-1, 3, nbasis, nbasis]
     rij_t:     nd::Array4<c64>,     // [nsw-1, 3, nbasis, nbasis]
     proj_t:    nd::Array4<f64>,     // [nsw-1, nbasis, nions, nproj]
-    efield:    Option<Efield>,
-
-    hamil:     nd::Array2<c64>,     // [nbasis, nbasis]
+    efield:    Option<Efield<'a>>,
 }
 
 
-impl<'a> Hamiltonian<'a> for SPHamiltonian {
+impl<'a> Hamiltonian<'a> for SPHamiltonian<'a> {
     type ConfigType   = HamilConfig;
     type CouplingType = Nac;
 
@@ -54,7 +49,60 @@ impl<'a> Hamiltonian<'a> for SPHamiltonian {
 
     fn from_h5<P>(fname: P) -> Result<Self>
     where P: AsRef<Path> {
-        todo!()
+        let f = H5File::open(fname)?;
+
+        let ikpoint  = f.dataset("ikpoint")?.read_scalar::<usize>()?;
+        let basis_up = f.dataset("basis_up")?.read_scalar::<[usize;2]>()?;
+        let basis_dn = f.dataset("basis_dn")?.read_scalar::<[usize;2]>()?;
+        let nbasis   = f.dataset("nbasis")?.read_scalar::<usize>()?;
+        let potim    = f.dataset("potim")?.read_scalar::<f64>()?;
+        let nsw      = f.dataset("nsw")?.read_scalar::<usize>()?;
+        let temperature = f.dataset("temperature")?.read_scalar::<f64>()?;
+
+        let eig_t: nd::Array2<f64> = f.dataset("eig_t")?.read()?;
+        let nac_t = {
+            let nac_t_r: nd::Array3<f64> = f.dataset("nac_t_r")?.read()?;
+            let nac_t_i: nd::Array3<f64> = f.dataset("nac_t_i")?.read()?;
+            nac_t_r.mapv(|v| c64::new(v, 0.0)) + nac_t_i.mapv(|v| c64::new(0.0, v))
+        };
+        let pij_t = {
+            let pij_t_r: nd::Array4<f64> = f.dataset("pij_t_r")?.read()?;
+            let pij_t_i: nd::Array4<f64> = f.dataset("pij_t_i")?.read()?;
+            pij_t_r.mapv(|v| c64::new(v, 0.0)) + pij_t_i.mapv(|v| c64::new(0.0, v))
+        };
+        let rij_t = {
+            let rij_t_r: nd::Array4<f64> = f.dataset("rij_t_r")?.read()?;
+            let rij_t_i: nd::Array4<f64> = f.dataset("rij_t_i")?.read()?;
+            rij_t_r.mapv(|v| c64::new(v, 0.0)) + rij_t_i.mapv(|v| c64::new(0.0, v))
+        };
+        let proj_t: nd::Array4<f64> = f.dataset("proj_t")?.read()?;
+
+        let efield = {
+            if f.dataset("efield").is_err() {
+                None
+            } else {
+                let raw: Vec<u8> = f.dataset("efield")?.read_raw()?;
+                let efield_src = String::from_utf8(raw)?;
+                Some(Efield::from_str(&efield_src)?)
+            }
+        };
+
+        Ok(SPHamiltonian {
+            ikpoint,
+            basis_up,
+            basis_dn,
+            nbasis,
+            potim,
+            nsw,
+            temperature,
+
+            eig_t,
+            nac_t,
+            pij_t,
+            rij_t,
+            proj_t,
+            efield
+        })
     }
 
     fn save_to_h5<P>(&self, fname: P) -> Result<()>
@@ -83,8 +131,9 @@ impl<'a> Hamiltonian<'a> for SPHamiltonian {
         f.new_dataset_builder().with_data(&self.proj_t).create("proj_t")?;
 
         if let Some(efield) = self.efield.as_ref() {
+            f.new_dataset_builder().with_data(efield.get_src()).create("efield");
         }
-        
-        todo!()
+
+        Ok(())
     }
 } 
